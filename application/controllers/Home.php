b05e4e636a358046967d95d19bae3985e8662166
Raphael Oliveira Cunha <raphael_arcanjo@live.com>
2019-10-09 15:48:40 -0300

    Criação dos personagens e armas

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,8 +2,42 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+	public function __construct()
+  {
+      parent::__construct();
+  }
+
 	public function index()
 	{
-		$this->load->view('home');
+		$personagens = array('humano','orc');
+		$personagens['humano'] = array(
+			'pv' => 12,
+			'for' => 1,
+			'agi' => 2
+		);
+		$personagens['orc'] = array(
+			'pv' => 20,
+			'for' => 2,
+			'agi' => 0
+		);
+		$armas = array('espada_longa','clava_madeira');
+		$armas['espada_longa'] = array(
+			'atq' => 2,
+			'def' => 1,
+			'dano' => rand(1,6)
+		);
+		$armas['clava_madeira'] = array(
+			'atq' => 1,
+			'def' => 0,
+			'dano' => rand(1,8)
+		);
+		$data['h_iniciativa'] = rand(1,20) + $personagens['humano']['agi'];
+		$data['o_iniciativa'] = rand(1,20) + $personagens['orc']['agi'];
+		var_dump($data['h_iniciativa']);
+		var_dump($data['o_iniciativa']);die;
+		$data['personagens'] = $personagens;
+		$data['armas'] = $armas;
+		$data['page_title'] = 'Batalha RPG';
+		$this->load->view('home',$data);
 	}
 }
